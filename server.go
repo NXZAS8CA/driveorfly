b99c22364b5665edfa9342b9f8c76e51e8e75484
NXZAS8CA <simon.krieger@mailbox.org>
2021-11-15 17:43:35 +0100

    added country list selection hardcode in form.html

--- server.go
+++ server.go
@@ -27,7 +27,7 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
-
+	http.HandleFunc("/form", formHandler)
 	fmt.Printf("Starting server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
