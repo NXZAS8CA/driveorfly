1156ac17f110b5256801798171854d1312f77454
NXZAS8CA <simon.krieger@mailbox.org>
2021-11-19 18:55:09 +0100

    started working on routing api

--- server.go
+++ server.go
@@ -4,17 +4,20 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func formHandler(w http.ResponseWriter, r *http.Request) {
 	//api handler call for current city
-	currentCityLatitude, currentCityLongitude := getCityCoordinations(getCityInformationURL(r.FormValue("currentCity")))
-	fmt.Println(currentCityLatitude, currentCityLongitude)
+	originCityLatitude, originCityLongitude := getCityCoordinations(getCityInformationURL(r.FormValue("currentCity")))
+	fmt.Println(originCityLatitude, originCityLongitude)
 
 	//api handler call for destination city
 	destinationCityLatitude, destinationCityLongitude := getCityCoordinations(getCityInformationURL(r.FormValue("destinationCity")))
 	fmt.Println(destinationCityLatitude, destinationCityLongitude)
 
+	request := fmt.Sprintf("https://router.hereapi.com/v8/routes?transportMode=car&origin=%f,%f&destination=%f,%f&return=summary", originCityLatitude, originCityLongitude, destinationCityLatitude, destinationCityLongitude)
+	fmt.Println(request)
 }
 
 func main() {
@@ -22,7 +25,11 @@ func main() {
 
 	http.Handle("/", fileServer)
 	http.HandleFunc("/form", formHandler)
+
+	fmt.Println(os.Getenv("HERE_Routing_API"))
+
 	fmt.Printf("Starting server at port 8080\n")
+
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
